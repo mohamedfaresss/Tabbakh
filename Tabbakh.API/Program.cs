4fbf02195f065d68fe1be84932882b6d002e3a15
mohamedfaresss <mohamedgamal1999920@gmail.com>
2026-03-28 19:33:24 +0200

    Initialize Tabbakh solution with clean architecture structure and base projects

--- Tabbakh.API/Program.cs
+++ Tabbakh.API/Program.cs
@@ -10,6 +10,9 @@ namespace Tabbakh.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
